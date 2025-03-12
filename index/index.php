e425787841893bce0db509a52d1b736b988df845
Jin Cyril Baydo <152479532+jnrku527@users.noreply.github.com>
2025-03-12 15:07:50 +0800

    Add files via upload

---
+++ index/index.php
@@ -0,0 +1,20 @@
+<?php 
+// Include the header
+include 'includes/header.php'; 
+?>
+<div class="container">
+    <!-- Jumbotron for the landing page -->
+    <div class="jumbotron mt-4">
+        <h1 class="display-4">Welcome RAMS!</h1>
+        <p class="lead">Manage and borrow digital assets with ease.</p>
+        <hr class="my-4">
+        <p>Explore our equipments and start borrowing from us.</p>
+        <a class="btn btn-primary btn-lg" href="/hiRAMan/borrowing/borrow_asset.php" role="button">Borrow an Item</a>
+        <a class="btn btn-primary btn-lg" href="/hiRAMan/borrowing/return_asset.php" role="button">Return an Item</a>
+    
+    </div>
+</div>
+<?php 
+// Include the footer.
+include 'includes/footer.php'; 
+?>
